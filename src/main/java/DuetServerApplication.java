5cd290e3ed2778d94f6b66dfe544164c80116725
Colin Sivak <crs4665@rit.edu>
2019-02-16 14:08:25 -0500

    Created routes that do nothing right now

--- src/main/java/DuetServerApplication.java
+++ src/main/java/DuetServerApplication.java
@@ -1,18 +1,27 @@
+import spark.Request;
+import spark.Response;
 import spark.Spark;
 
 public class DuetServerApplication {
-    private static String onRecievePOSTSignup() {
+    private Object onRecievePOSTSignup(Request request, Response response) {
         return null;
     }
 
-    private static String onRecieveGETCards() {
+    private Object onRecieveGETCards(Request request, Response response) {
         return null;
     }
 
-    private static
+    private Object onRecievePOSTVote(Request request, Response response) {
+        return null;
+    }
 
     public DuetServerApplication() {
-
+        System.out.println("Creating signup route");
+        Spark.post("/signup", this::onRecievePOSTSignup);
+        System.out.println("Creating get cards route");
+        Spark.get("/cards", this::onRecieveGETCards);
+        System.out.println("Creating vote route");
+        Spark.post("/vote", this::onRecievePOSTVote);
     }
 
     public static void main(String[] args) {
