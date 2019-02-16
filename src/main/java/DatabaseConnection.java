d94d37474a642399f55593921cc9ea78b9511fb6
Dan Lennon <djl9490@rit.edu>
2019-02-16 14:20:36 -0500

    User class edits

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,2 +1,20 @@
+import com.bigchaindb.builders.BigchainDbConfigBuilder;
+
 public class DatabaseConnection {
+
+    /*
+     * First Name - Last Name - Username - School
+     */
+    private static void test() {
+        BigchainDbConfigBuilder.baseUrl("duetdating.net").
+                addToken("First Name", )
+
+    }
+
+    /**
+     * @return FirstName
+     */
+    private static String getFirst(){
+
+    }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,14 @@
 public class User {
+
+    private User(){
+        String firstName;
+        String lastName;
+        String userName;
+        String school;
+    }
+
+
+    // create user upload to db return user
+
+    //
 }
